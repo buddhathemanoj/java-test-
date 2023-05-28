c34e4c5056d133fa36757ffbd4e793d792c4107c
buddhathemanoj <134469946+buddhathemanoj@users.noreply.github.com>
2023-05-28 07:26:04 -0700

    Add files via upload

---
+++ java-test-/java-test-/untitled/src/Maian.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.util.Scanner;
+
+/*  public class Maian {
+    public static void main(String[] ars){
+        int Bike =20000;
+        int Car = 200000;
+        if(Car>Bike){
+            System.out.println("Car will cost more than bike");
+        }
+    }
+}
+                                                                       */
+
+
+public class Maian {
+    public static void main(String[] ars) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number  ");
+        double a = scanner.nextInt();
+
+        System.out.println(a+ " your number");
+
+        scanner.close();
+    }
+}
+
